ce992fb55d4db78a8bdbc7a100f1c914ef0b7337
Daniel <42379129+danielirano@users.noreply.github.com>
2020-11-30 08:03:09 -0300

    Update curso.js

--- app/controllers/curso.js
+++ app/controllers/curso.js
@@ -1,12 +1,12 @@
 const curso = require("../models/curso");
 
 module.exports = function (app) {
-    /// cria a variável que vai conter as funções da controller
+    
     var controller = {};
-    // cria variável que irá acessar a model
+    
     var curso = app.models.curso;
 
-    // cria a função de cadastrar Curso
+    
     controller.salvarCurso = function (req, res) {
         curso.create(req.body).then(
             function (curso) {
@@ -17,13 +17,13 @@ module.exports = function (app) {
             }
         );
     }
-    // função que retorna os cursos cadastrados
+    
     controller.listaCursos = function (req, res) {
-        // executa um find para retornar os cursos
+    
         curso.find().exec().then(
-            //em caso de sucesso
+    
             function (cursos) {
-                const response = cursos.map(cur => { // metado map ele percorre o array e organiza seus atributos.
+                const response = cursos.map(cur => { 
                     return {
                         id: cur._id,
                         nome: cur.nome,
@@ -43,7 +43,7 @@ module.exports = function (app) {
         );
     }
 
-    // atualiza cursos já cadastrados
+    
     controller.alteraCurso = function (req, res) {
         var _id = req.body._id;
         curso.findByIdAndUpdate(_id, req.body).exec().then(
@@ -55,11 +55,11 @@ module.exports = function (app) {
             }
         );
     }
-    // remove cursos cadastrados
+    
     controller.removeCurso = function (req, res) {
         var _id = req.params.id;
         curso.remove({ "_id": _id }).exec().then(
-            //em caso de sucesso
+            
             function (curso) {
                 res.status(204).end();
             },
@@ -70,11 +70,11 @@ module.exports = function (app) {
             }
         );
     }
-    //Retorna somente um curso
+    
     controller.obtemCurso = function (req, res) {
         var _id = req.params.id;
         curso.findById(_id).exec().then(
-            //sucesso
+    
             function (curso) {
                 if (!curso) {
                     res.status(404).end();
@@ -91,7 +91,7 @@ module.exports = function (app) {
                     res.status(200).json(response);
                 }
             }, 
-            //erro
+    
             function (erro) {
                 console.error(erro);
                 res.status(500).json(erro);
